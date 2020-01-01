cb05acb9d64b0b64f51ef22a8f37d1f11080517d
Geoffrey Bauduin <bauduin.geo@gmail.com>
2020-01-01 17:52:05 +0100

    fix: schema validation made with definitions in another file

--- cmd/raid-codex-cli/schema_validate/schema_validate.go
+++ cmd/raid-codex-cli/schema_validate/schema_validate.go
@@ -2,6 +2,8 @@ package schema_validate
 
 import (
 	"fmt"
+	"io/ioutil"
+	"strings"
 
 	"github.com/raid-codex/tools/utils"
 	"github.com/xeipuuv/gojsonschema"
@@ -9,24 +11,35 @@ import (
 )
 
 type Command struct {
-	File       *string
-	SchemaFile *string
+	File        *string
+	SchemaFile  *string
+	Definitions *string
 }
 
 func New(cmd *kingpin.CmdClause) *Command {
 	return &Command{
-		File:       cmd.Flag("file", "Filename to check").Required().String(),
-		SchemaFile: cmd.Flag("schema-file", "Filename for the schema").Required().String(),
+		File:        cmd.Flag("file", "Filename to check").Required().String(),
+		SchemaFile:  cmd.Flag("schema-file", "Filename for the schema").Required().String(),
+		Definitions: cmd.Flag("definitions", "Filename for schema definitions").Required().String(),
 	}
 }
 
 func (c *Command) Run() {
-	schemaLoader := gojsonschema.NewReferenceLoader(*c.SchemaFile)
+	definitions, err := ioutil.ReadFile(*c.Definitions)
+	if err != nil {
+		utils.Exit(1, err)
+	}
+	schema, err := ioutil.ReadFile(*c.SchemaFile)
+	if err != nil {
+		utils.Exit(1, err)
+	}
+	schemaString := strings.Replace(string(schema), `"definitions": {}`, fmt.Sprintf(`"definitions": %s`, string(definitions)), 1)
+	schemaLoader := gojsonschema.NewStringLoader(schemaString)
 	documentLoader := gojsonschema.NewReferenceLoader(*c.File)
 
 	result, err := gojsonschema.Validate(schemaLoader, documentLoader)
 	if err != nil {
-		utils.Exit(1, err)
+		utils.Exit(1, fmt.Errorf("cannot validate: %s", err))
 	}
 
 	if result.Valid() {

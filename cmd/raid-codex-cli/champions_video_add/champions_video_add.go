450f420f70e9979f20c1d1af13d589a2c23c6f3e
Geoffrey Bauduin <bauduin.geo@gmail.com>
2020-05-07 14:40:56 +0200

    fix: date of videos + dash at the beginning of video ID

--- cmd/raid-codex-cli/champions_video_add/champions_video_add.go
+++ cmd/raid-codex-cli/champions_video_add/champions_video_add.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/raid-codex/tools/common"
@@ -17,6 +18,7 @@ type Command struct {
 	Author        *string
 	VideoID       *string
 	Source        *string
+	PublishedAt   *string
 }
 
 func New(cmd *kingpin.CmdClause) *Command {
@@ -26,6 +28,7 @@ func New(cmd *kingpin.CmdClause) *Command {
 		Author:        cmd.Flag("author", "Video author").Required().String(),
 		VideoID:       cmd.Flag("video-id", "Video ID").Required().String(),
 		Source:        cmd.Flag("source", "Source").Required().String(),
+		PublishedAt:   cmd.Flag("published-at", "Published at").String(),
 	}
 	return command
 }
@@ -38,11 +41,22 @@ func (c *Command) Run() {
 	if errChampion != nil {
 		utils.Exit(1, errChampion)
 	}
+	var date string
+	if c.PublishedAt != nil && *c.PublishedAt != "" {
+		date = *c.PublishedAt
+	} else {
+		date = time.Now().Format(time.RFC3339)
+	}
+	videoID := *c.VideoID
+	if strings.HasPrefix(videoID, `\-`) {
+		// hax
+		videoID = videoID[1:]
+	}
 	champion.Videos = append(champion.Videos, &common.Video{
 		Author:    *c.Author,
 		Source:    *c.Source,
-		ID:        *c.VideoID,
-		DateAdded: time.Now().Format(time.RFC3339),
+		ID:        videoID,
+		DateAdded: date,
 	})
 	if errSanitize := champion.Sanitize(); errSanitize != nil {
 		utils.Exit(1, errSanitize)
